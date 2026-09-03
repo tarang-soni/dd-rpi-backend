77b9294ba75a01814f27f9c6ea31edec04aaf5d1
tarang-soni <83337255+tarang-soni@users.noreply.github.com>
2026-09-04 00:10:37 +0530

    fixed server text

--- VideoStreamer.cpp
+++ VideoStreamer.cpp
@@ -48,7 +48,7 @@ bool dd_rpi_backend::VideoStreamer::start(const std::string& clientIp, int port)
     pipelineString =
     "libcamerasrc ! "
     "video/x-raw,width=640,height=480,framerate=15/1 ! "
-    "jpegenc quality=75 ! "
+    "jpegenc quality=60 ! "
     "rtpjpegpay pt=26 ! "
     "udpsink host=" + clientIp +
     " port=" + std::to_string(port) +
